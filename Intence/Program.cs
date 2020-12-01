2b3b09041438054dca2557f3ad8661c6d523af74
Леонид Шайхутдинов <leovbox@mail.ru>
2020-12-01 08:28:12 +0500

    Добавьте файлы проекта.

---
+++ Intence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
